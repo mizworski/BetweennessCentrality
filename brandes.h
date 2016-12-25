3308524eceb233f1bc549b5276aeb174838c52b9
Michał Izworski <m.izworski7@gmail.com>
2016-12-25 22:28:40 +0100

    Input and output of program done.

---
+++ brandes.h
@@ -0,0 +1,32 @@
+//
+// Created by michal on 12/25/16.
+//
+
+#ifndef PW2_BRANDES_H
+#define PW2_BRANDES_H
+
+typedef std::set<int> vertices;
+typedef std::vector<int> neighbours;
+typedef std::map<int, neighbours> edges;
+typedef std::map<int, double> results;
+
+void write_results(edges &neighbourhood, results &results, std::string &output_filename);
+
+void read_graph(vertices &actors, edges &neighbourhood, std::string &filename);
+
+void calculate_betweenness_centrality(vertices &actors, edges &neighbourhood, results &bc);
+
+void calculate_betweenness_centrality(const int actor,
+                                      const vertices &actors,
+                                      const edges &neighbourhood,
+                                      results &bc);
+
+void thread_do(std::deque<int> &vertices_to_process,
+               const vertices &actors,
+               const edges &neighbourhood,
+               std::promise<results> &result,
+               std::mutex &mutex);
+
+void update_results(results &bc, results &result);
+
+#endif //PW2_BRANDES_H
